035ae2ee4b617fb0a0d1f48c2cc676fcea512117
Анна Догаева <dogaevaanna@WebStroy.local>
2019-08-23 18:28:44 +0300

    Создан файл queries.sql, начало работы над заданием

--- schema.sql
+++ schema.sql
@@ -6,53 +6,53 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 
-DROP TABLE IF EXISTS `bids`;
-DROP TABLE IF EXISTS `lots`;
-DROP TABLE IF EXISTS `users`;
-DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS bids;
+DROP TABLE IF EXISTS lots;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
 
-CREATE TABLE `categories` (
-    `id` INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` CHAR(64) UNIQUE NOT NULL,
-    `symbol_code` CHAR(64) UNIQUE NOT NULL
+CREATE TABLE categories (
+    id INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(64) UNIQUE NOT NULL,
+    symbol_code CHAR(64) UNIQUE NOT NULL
 );
 
-CREATE TABLE `lots`
+CREATE TABLE lots
 (
-    `id` INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    `start_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `name` CHAR(64) NOT NULL,
-    `description` CHAR(255) NOT NULL,
-    `image` CHAR(255) NOT NULL,
-    `start_price` INT(12)  NOT NULL,
-    `end_date` TIMESTAMP NOT NULL,
-    `bid_step` INT(12)  NOT NULL,
-    `author_id` INT(12)  NOT NULL,
-    `winner_id` INT(12),
-    `category_id` INT(12)  NOT NULL
+    id INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    start_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name CHAR(64) NOT NULL,
+    description CHAR(255) NOT NULL,
+    image CHAR(255) NOT NULL,
+    start_price INT(12)  NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    bid_step INT(12)  NOT NULL,
+    author_id INT(12)  NOT NULL,
+    winner_id INT(12),
+    category_id INT(12)  NOT NULL
 );
 
-CREATE TABLE `bids`
+CREATE TABLE bids
 (
-    `id` INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `sum` INT(12)  NOT NULL,
-    `author_id` INT(12)  NOT NULL,
-    `lot_id` INT(12)  NOT NULL
+    id INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sum INT(12)  NOT NULL,
+    author_id INT(12)  NOT NULL,
+    lot_id INT(12)  NOT NULL
 );
 
-CREATE TABLE `users`
+CREATE TABLE users
 (
-    `id` INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    `register_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `email` CHAR(64) UNIQUE NOT NULL,
-    `name` CHAR(64) NOT NULL,
-    `password` CHAR(64) NOT NULL,
-    `avatar` CHAR(64),
-    `contact_info` CHAR(255) NOT NULL
+    id INT(12)  AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email CHAR(64) UNIQUE NOT NULL,
+    name CHAR(64) NOT NULL,
+    password CHAR(64) NOT NULL,
+    avatar CHAR(64),
+    contact_info CHAR(255) NOT NULL
 );
 
-CREATE UNIQUE INDEX `category_name` ON `categories`(`name`);
-CREATE INDEX `lot_name` ON lots(`name`);
-CREATE INDEX `bid_sum` ON bids(`sum`);
-CREATE INDEX `user_name` ON users(`name`);
+CREATE UNIQUE INDEX category_name ON categories(name);
+CREATE INDEX lot_name ON lots(name);
+CREATE INDEX bid_sum ON bids(sum);
+CREATE INDEX user_name ON users(name);
