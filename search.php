8e9fb892c6a2f7d6d2499fb0a8bb0175ffe9830e
Анна Догаева <dogaevaanna@WebStroy.local>
2019-09-23 14:29:59 +0300

    пагинация

--- search.php
+++ search.php
@@ -7,31 +7,32 @@ $categories = get_categories($con);
 
 $lots = [];
 $search = $_GET['search'] ?? '';
+$search = trim($search);
 
-if ($search) {
-    $sql = "SELECT l.NAME, l.start_price, l.image, l.end_date, l.id, c.name FROM lots l JOIN categories c ON l.category_id = c.id WHERE MATCH(l.NAME, l.description) AGAINST(?)";
+if($search) {
+    $cur_page = $_GET['page'] ?? 1;
+    $page_items = 9;
 
+    //получаем кол-во лотов, соответствующих поисковому запросу
+    $sql = 'SELECT COUNT(*) as cnt FROM lots WHERE MATCH(NAME, description) AGAINST(?)';
+    $stmt = db_get_prepare_stmt($con, $sql, [$search]);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $items_count = mysqli_fetch_assoc($result)['cnt'];
+
+    $pages_count = ceil($items_count / $page_items); //кол-во страниц пагинации
+    $offset = ($cur_page - 1) * $page_items; //смещение
+
+    $pages = range(1, $pages_count); //массив с номерами страниц
+
+    //получаем информации по лотам для вывода постранично
+    $sql = 'SELECT l.NAME, l.start_price, l.image, l.end_date, l.id, c.name FROM lots l JOIN categories c ON l.category_id = c.id WHERE MATCH(l.NAME, l.description) AGAINST(?) LIMIT ' . $page_items . ' OFFSET ' . $offset;
     $stmt = db_get_prepare_stmt($con, $sql, [$search]);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
     $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
 }
-
-$cur_page = $_GET['page'] ?? 1;
-$page_items = 2;
-
-$result = mysqli_query($con, "SELECT COUNT(*) as cnt FROM lots");
-$items_count = mysqli_fetch_assoc($result)['cnt'];
-
-$pages_count = ceil($items_count / $page_items);
-$offset = ($cur_page - 1) * $page_items;
-
-$pages = range(1, $pages_count);
-
-$sql = 'SELECT l.NAME, l.start_price, l.image, l.end_date, l.id, c.name FROM lots l JOIN categories c ON l.category_id = c.id WHERE MATCH(l.NAME, l.description) AGAINST(?)' . ' LIMIT ' . $page_items . ' OFFSET ' . $offset;
-
 
 $page_content = include_template('searchpage.php', [
     'categories' => $categories,
--- templates/searchpage.php
+++ templates/searchpage.php
@@ -15,7 +15,7 @@
             <?php foreach ($lots as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= htmlspecialchars($lot['image']); ?>" width="350" height="260" alt="<?=$lot['name'] ?>">
+                        <img src="/<?= htmlspecialchars($lot['image']); ?>" width="350" height="260" alt="<?=$lot['name'] ?>">
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?= htmlspecialchars($lot['name']); ?></span>
@@ -42,7 +42,7 @@
     <ul class="pagination-list">
         <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
         <?php foreach ($pages as $page): ?>
-            <li class="pagination-item <?php if ($page == $cur_page): ?>pagination-item-active<?php endif; ?>"><a href="search.php/?page=<?=$page;?>"><?=$page;?></a></li>
+            <li class="pagination-item <?php if ($page == $cur_page): ?>pagination-item-active<?php endif; ?>"><a href="/search.php/?search=<?= $search; ?>&find=Найти&page=<?= $page; ?>"><?=$page;?></a></li>
         <?php endforeach; ?>
         <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
     </ul>
